62172eb075efb3c31e2e619fc4e21e6a9d84affe
danny <dannyorh@gmail.com>
2011-12-12 21:49:26 -0800

    install.sh now does not run snippets

--- UtilTools/dotFiles/install.sh
+++ UtilTools/dotFiles/install.sh
@@ -2,6 +2,6 @@
 dir=`dirname $0`
 cp $dir/.gitconfig ~/.gitconfig
 cp $dir/.bash_profile ~/.bash_profile
-cp $dir/.gitcommit.snippets ~/vimfiles/bundle/snipmate.vim/snippets/gitcommit.snippets
+#cp $dir/.gitcommit.snippets ~/vimfiles/bundle/snipmate.vim/snippets/gitcommit.snippets
 
 . ~/.bash_profile
